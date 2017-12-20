1f5af7ed2e394f88af4e92fc95eed553eabef2c1
Andreas Bielawski <brawl98@outlook.com>
2017-12-20 18:54:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Local Fonts
+============
+This is a WordPress plugin which is coded by the WiiDatabase Team. It is specifically for the TwentyThirteen theme:
+1. It removes all calls to Google Fonts server (preconnect too)
+2. It loads all fonts local
+
+You can customize the plugin to your liking! Use the [Google WebFonts Helper](https://google-webfonts-helper.herokuapp.com/fonts) to download your fonts and create the right CSS. Then, just adjust the CSS and the fonts folder.
---
+++ local-fonts.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name:  Local Fonts
+Plugin URI:   https://github.com/WiiDatabase/Local-Fonts
+Description:  Host any fonts locally
+Version:      1.0
+Author:       iCON
+Author URI:   https://wiidatabase.de
+License:      MIT
+License URI:  https://opensource.org/licenses/MIT
+*/
+
+function local_fonts_styles() {
+        wp_enqueue_style( 'local-fonts', plugin_dir_url( __FILE__ ) . 'assets/css/local-fonts.min.css' );
+}
+add_action( 'wp_enqueue_scripts', 'local_fonts_styles' );
+
+function remove_google_fonts() {
+    wp_deregister_style('twentythirteen-fonts');
+}
+add_action('wp_enqueue_scripts','remove_google_fonts',100);
+
+function remove_google_fonts_connect(){
+	remove_filter('wp_resource_hints', 'twentythirteen_resource_hints');
+}
+
+add_action( 'after_setup_theme', 'remove_google_fonts_connect' );
+?>
